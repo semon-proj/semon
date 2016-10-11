5fe40aacf31925f1fdcc921ebb7e8042b248a91a
kaiye <1252242781@qq.com>
2016-10-11 14:36:31 +0800

    项目转移

---
+++ qingmo/common/js/lunbo.js
@@ -0,0 +1,62 @@
+(function(){
+    var nowimg=0;
+        
+        var $rightBtn= $(".rightBtn")
+        var $leftBtn= $(".leftBtn")
+        var lis=$(".banner-in li")
+        var $banner=$(".banner")
+        var $circle=$(".circle span")
+        var animateTime=300;
+        var timer=null;
+        var setTimer=3200;
+
+        var $bannerIn=$(".banner .banner-in")
+        $(".banner-in>li:first").clone().appendTo(".banner-in")
+        // rightBtn
+        $rightBtn.click(rightBtnfunc)
+            function rightBtnfunc() {
+            if(!$bannerIn.is(":animated")){
+                if(nowimg<lis.length-1){
+                nowimg++
+                putong();
+            }else{
+                nowimg=0;
+                $bannerIn.animate({"left":lis.length*-430}, animateTime,function(){
+                   $bannerIn.css("left",0)
+                 })
+            }
+            $circle.eq(nowimg).addClass('cur').siblings().removeClass("cur")
+            }
+        }
+        // leftBtn
+        $leftBtn.click(function() {
+          if(!$bannerIn.is(":animated")){
+            if(nowimg>0){
+            nowimg--
+            putong();
+          }else{
+            nowimg=lis.length-1
+            $bannerIn.css("left",lis.length*-430)
+           putong();
+          }
+           $circle.eq(nowimg).addClass('cur').siblings().removeClass("cur")
+          }
+        });
+        // 小圆点业务
+        $circle.mouseover(function() {
+            nowimg=$(this).index()
+            $bannerIn.animate({"left":nowimg*-430}, animateTime)
+             $circle.eq(nowimg).addClass('cur').siblings().removeClass("cur")
+        });
+        function putong(){
+            $bannerIn.animate({"left":nowimg*-430}, animateTime)
+        }
+        timer=setInterval(rightBtnfunc,setTimer)
+        $banner.mouseenter(function() {
+           clearInterval(timer)
+        });
+        $banner.mouseleave(function() {
+           clearInterval(timer)
+           timer=setInterval(rightBtnfunc,setTimer)
+        });
+        })();
---
+++ qingmo/common/js/nav.js
@@ -0,0 +1,70 @@
+/* 
+* @Author: kaiye
+* @Date:   2016-10-09 19:03:58
+* @Last Modified by:   kaiye
+* @Last Modified time: 2016-10-10 10:02:28
+*/
+
+jQuery(document).ready(function ($){
+
+//顶部导航菜单
+    $('.nav-search').click(function (){
+        $('.nav-search-bar').toggle('fast');
+        $('body').one('click', function (){$('.nav-search-bar').hide('fast');});
+        // return false;
+    });   
+    $('.nav-point').click(function (){
+        $('.nav-point-list').toggle('fast');
+        $('body').one('click', function (){$('.nav-point-list').hide('fast');});
+        return false;
+    });
+    $('.denglu').click(function (){
+        $('.denglu-list').toggle('fast');
+        $('body').one('click', function (){$('.denglu-list').hide('fast');});
+        return false;
+    });
+    $('.denglu-list').click(function(){return false;});
+    $('.nav-point-list').click(function(){return false;});
+    $('.nav-search-bar').click(function(){return false;});
+    $('.nav-switch').click(function(){$('.nav-menu').slideToggle('fast');});
+    $('.nav-switch').toggle(function(){$(this).css('background', '#6400A6').text('收起菜单');},
+    function(){$(this).removeAttr("style").text('导航菜单');});
+    $('.nav-menu ul li').hover(function (){
+        var e = $(this);
+        t = setTimeout(function (){
+            e.children('ul').slideDown('fast');
+        }, 300);
+    },
+    function (){clearTimeout(t);$(this).children('ul').slideUp('fast');});
+    $(window).resize(function(){widthJudge();})
+    function widthJudge(){var winWidth = $(window).width();if (winWidth>750){ $('.nav-menu').fadeIn('normal'); }}
+
+    $("a,.tip").not("a.detail").each(function() {
+        function c(a) {
+            var c = document.body.clientWidth - a.pageX,
+                d = $(".mtitle").outerWidth();
+            d + 18 > c ? (x = d + 6, $(".mtitle").css({
+                top: a.pageY - b + "px",
+                left: a.pageX - x + "px",
+                position: "absolute"
+            })) : (x = -16, $(".mtitle").css({
+                top: a.pageY - b + "px",
+                left: a.pageX - x + "px",
+                position: "absolute"
+            }))
+        }
+        var a, b;
+        this.title && (a = this.title, b = -24, $(this).mouseover(function(b) {
+            this.title = "",
+                $("body").append('<div class="mtitle" style="max-width:300px;color:#333;background:#f6f6f6;border:2px solid #ddd;padding:4px 12px;box-shadow:2px 3px 6px rgba(0,0,0,.5);z-index:999;display:none;">' + a + "</div>"),
+                c(b),
+                $(".mtitle").slideDown("fast")
+        }).mouseout(function() {
+            this.title = a,
+                $(".mtitle").remove()
+        }).mousemove(function(a) {
+            c(a)
+        }))
+    });
+
+})
