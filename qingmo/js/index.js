b2f8c5dc43ee7f0cb5521991f0b3410bf58af98a
lujiaolancode <342240955@qq.com>
2016-10-14 21:21:01 +0800

    首页移动文字bug解决完成上传

--- qingmo/js/index.js
+++ qingmo/js/index.js
@@ -31,10 +31,35 @@ $(function () {
                 }
                 $article.appendTo($div);
             });
+            $(".thumbnail a").each(function () {
 
+                $(this).mouseenter(function (e) {
+                    // alert(e.offsetX);
+                    // alert(e.offsetY);
+                    aTip = this.title;
+                    this.title = "";
+//                            console.log(aTip);
+                    $aTip = $("<div class='clear'></div> <div id='aTip'>" + aTip + "</div> <div class='clear'></div> ");
+//                            console.log($aTip);
+                    $aTip.appendTo("body");
+                    $("#aTip").css({
+//                                距离计算问题,弹出的框显示的位置
+                        top: (e.pageY+20) + "px",
+                        left: (e.pageX+30) + "px"
+                    }).show();
+                }).mouseleave(function () {
+                    this.title = aTip;
+                    $aTip.remove();
+                }).mousemove(function (e) {
+                    $("#aTip").css({
+                        position: "absolute",
+                        top: (e.pageY+20) + "px",
+                        left: (e.pageX+30) + "px"
+                    });
+                })
+            });
         }
     });
-    var x = 30;
     $(".thumbnail a").each(function () {
 
         $(this).mouseenter(function (e) {
