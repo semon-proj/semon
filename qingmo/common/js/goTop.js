20df496ef9cf4c498012da4d6b6b1db5efa6f1af
yyh <931147735@qq.com>
2016-10-17 21:05:47 +0800

    回到顶部按钮

---
+++ qingmo/common/js/goTop.js
@@ -0,0 +1,29 @@
+
+
+
+
+$(function(){
+    var $topBtn = $('#gotop');
+    var isTopBtnShow = false;
+    window.onscroll = function() {
+        var t = document.documentElement.scrollTop || document.body.scrollTop;
+//				console.log(t);
+        if (t >= 800 && !isTopBtnShow) {
+            $topBtn.css("display", "block");
+            isTopBtnShow = true;
+        } else if(t < 800 && isTopBtnShow) {
+            $topBtn.css("display", "none");
+            isTopBtnShow = false;
+        }
+    }
+    //点击id为go_top的元素时网页回到顶部
+
+    $("#gotop").click(function(){
+
+        $('html,body').animate({scrollTop:0},1000);//回到顶端
+
+        return false;
+
+    });
+
+});
