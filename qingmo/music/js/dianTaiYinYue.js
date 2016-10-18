909447aeb993c499bffb51502f142b2baccdad54
kaiye <1252242781@qq.com>
2016-10-18 20:23:30 +0800

    电台音乐进一步完善

--- qingmo/music/js/dianTaiYinYue.js
+++ qingmo/music/js/dianTaiYinYue.js
@@ -2,6 +2,10 @@
  * Created by Administrator on 2016/10/11.
  */
 $(function () {
+    var flag = false;
+    var timer1 = null;
+    var audio = document.getElementById("audio");
+
     // 音乐电台列表项ajax获取
     $.ajax({
         url: "json/content.json",
@@ -54,23 +58,37 @@ $(function () {
                 }
             }).toggle(function () {
                 $(".album-item .ctl").remove();
-                $(".album-item .playR").remove();
+
                 $(".album-item .pauseR").remove();
-                $(".music_list").remove();
-                $('<i class="icon iconfont pauseR">&#xe607;</i>').appendTo(this).addClass("pasue_R");
-                // $(".music_list").css("display","block");
-                $(".nmplaybar").css("display","block");
+
+                if(($(this).children("i").is(".playR"))){
+                    audio.play();
+                    $('<i class="icon iconfont pauseR">&#xe607;</i>').appendTo(this).addClass("pasue_R");
+                    $(".nmplaybar").css("display","block");
+                    $(".music_list").css("display","block");
+                    $("#nmplayer-button span").remove();
+                    $("#nmplayer-button").append($("<span class='icon iconfont'>&#xe609;</span>"));
+
+                }else {
+
+                    // $(".music_list").remove();
+                    $('<i class="icon iconfont pauseR">&#xe607;</i>').appendTo(this).addClass("pasue_R");
+                    // $(".music_list").css("display","block");
+                    $(".nmplaybar").css("display","block");
                     var index_album_list = $(this).parent().index();
-                    //思想：用行*个数+index
-                var index = $(this).index();
-                var index_title = $(this)[0].outerText;
-                // console.log($(this));;
-                var $mus_list = $('<div class="music_list"><div class="mus_title">'+index_title+'</div> <ul class="mus_ul"></ul></div>');
-                $("#cont_wrapper").children().eq(index_album_list).after($mus_list);
-                musicList ((index_album_list)*5+(index+1));
-                $("#nmplayer-button span").remove();
-                $("#nmplayer-button").append($("<span class='icon iconfont'>&#xe609;</span>"));
-                },
+                        //思想：用行*个数+index
+                    var index = $(this).index();
+                    var index_title = $(this)[0].outerText;
+                    // console.log($(this));;
+                    var $mus_list = $('<div class="music_list"><div class="mus_title">'+index_title+'</div> <ul class="mus_ul"></ul></div>');
+                    $("#cont_wrapper").children().eq(index_album_list).after($mus_list);
+                    musicList ((index_album_list)*5+(index+1));
+                    $("#nmplayer-button span").remove();
+                    $("#nmplayer-button").append($("<span class='icon iconfont'>&#xe609;</span>"));
+
+                }
+                $(".album-item .playR").remove();
+            },
             function () {
                 $(".album-item .pauseR").remove();
                 $('<i class="icon iconfont playR">&#xe606;</i>').appendTo(this).addClass("play_R");
@@ -78,6 +96,7 @@ $(function () {
                 // $(".nmplaybar").css("display","none");
                 $("#nmplayer-button span").remove();
                 $("#nmplayer-button").append($("<span class='icon iconfont'>&#xe60b;</span>"));
+                audio.pause();
             })
         }
 
@@ -92,6 +111,8 @@ $(function () {
             method: "get",
             success: function (data) {
                 $.each(data, function (i, val) {
+                    // console.log(val);
+                    allTime = val.duration;
                     var seconds = Math.round(val.duration/jishu);
                     var min = Math.floor(seconds / 60);
                     if(min <= 9){
@@ -117,13 +138,72 @@ $(function () {
                 }).click(function () {
                     $(".mus_ul li").removeClass("mus_currentplay");
                     $(this).addClass("mus_currentplay");
-                })
+                    // alert($(this).text());
+                    var mus_str = $(this).text();
+                    var mus_str_length = mus_str.length;
+                    //下面字符串截取为什么是13和27，因为json数据里面取出来就是前后有空格，而且所有的都是这样，而前面有13个，后面有27个，所以这样截取
+                    var mus_str1 = mus_str.substring(13,mus_str_length-27);
+                    $(".nmplayer-title").text(mus_str1);
+                });
+                /*刚点开第一首播放时间*/
+                //清除上一首歌的定时器
+                // console.log();
+                if(flag == true){
+                    window.clearInterval(timer1);
+                    flag = false;
+                }
+                console.log(flag);
+                var mus_Url = data[0].mp3;
+                //字符串时间格式转秒格式
+                var mus_seconds = parseInt(data[0].duration.substring(0,2))*60+parseInt(data[0].duration.substring(3,5));
+                // console.log(mus_seconds);
+                playcontrol(mus_Url,mus_seconds);
+
             },
             error: function () {
                 alert("请求数据出错！");
                 return false;
             }
         })
+    }
+
+    function playcontrol(mus_url,mus_seconds) {
+        // console.log(mus_url);
+        flag = true;
+        $("#audio").attr({
+            "src":mus_url,
+            "autoplay":"autoplay"
+        });
+        timer1 = setInterval(function() {
+            var currentTime = audio.currentTime;
+            currentTime =parseInt(currentTime);
+            // console.log(currentTime);
+            // console.log(mus_seconds);
+            $(".nmplayer-prosess").css("width",(currentTime/mus_seconds)*100+'%');
+            if(currentTime == mus_seconds){
+                window.clearInterval(timer1);
+            }
+            $(".nmplayer-time").html(timeChange(currentTime));
+        }, 1000);
+    }
+
+    //播放时间格式化
+    function timeChange(time) {//默认获取的时间是时间戳改成我们常见的时间格式
+        var audio = document.getElementById('audio');
+        //分钟
+        var minute = time / 60;
+        var minutes = parseInt(minute);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        //秒
+        var second = time % 60;
+        seconds = parseInt(second);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        var allTime = "" + minutes + "" + ":" + "" + seconds + "";
+        return allTime;
     }
     
     //点击切换上一首
@@ -142,13 +222,15 @@ $(function () {
         $(".mus_ul li").eq(numb).addClass("mus_currentplay");
     });
 
-    //点击暂停
+    //点击暂停/播放
     $("#nmplayer-button").toggle(function () {
         $("#nmplayer-button span").remove();
         $("#nmplayer-button").append($("<span class='icon iconfont'>&#xe60b;</span>"));
+        audio.pause();
     },function () {
         $("#nmplayer-button span").remove();
         $("#nmplayer-button").append($("<span class='icon iconfont'>&#xe609;</span>"));
+        audio.play();
     });
 
     //点击加载更多
